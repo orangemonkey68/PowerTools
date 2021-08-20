550897dac5b8d847191b0220d6ea9a9c81f4baad
clare <clara@DESKTOP-SVLILSG>
2021-08-20 18:01:40 -0500

    Successfully added GH Packages as dependencies! Fabric module may need to be removed and re-added since it might not be parented correctly

---
+++ Fabric/src/main/java/me/orangemonkey68/powertools/fabric/PowerTools.java
@@ -0,0 +1,10 @@
+package me.orangemonkey68.powertools.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PowerTools implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
